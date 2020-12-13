5225f55e604d97601d14e0ef030e9301ee17b093
lin <1036190402@qq.com>
2020-12-13 21:12:15 +0800

    Initial commit

---
+++ src/day01/IfTest.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class IfTest {
+    public static void main(String[] args) {
+        int x = 2;
+        if(x == 3){
+            System.out.println("This is 3");
+        }else {
+            System.out.println("X is NOT 3");
+        }
+        System.out.println("this runs no matter what");
+    }
+}
