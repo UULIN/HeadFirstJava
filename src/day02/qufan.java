8c262e6a4ea097bb4b4cbdf92afaffd8531924e0
lin <1036190402@qq.com>
2020-12-24 00:49:43 +0800

    new

---
+++ src/day02/qufan.java
@@ -0,0 +1,23 @@
+/*
+ * qufan.java
+ * Copyright 2020 Qunhe Tech, all rights reserved.
+ * Qunhe PROPRIETARY/CONFIDENTIAL, any form of usage is subject to approval.
+ */
+
+package day02;
+
+/**
+ * Function: !取反
+ *
+ * @author 云铮
+ * @date 2020/12/24
+ */
+public class qufan {
+
+    public static void main(String[] args) {
+        int a = 1;
+
+        System.out.println(a == 1);
+        System.out.println(! (a == 1));
+    }
+}
