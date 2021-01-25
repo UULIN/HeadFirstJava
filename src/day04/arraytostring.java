bddaee9a535dd171eef4f23e4ea0edb128a9c28e
yunzheng <yunzheng@qunhemail.com>
2021-01-25 10:09:04 +0800

    new

---
+++ src/day04/arraytostring.java
@@ -0,0 +1,25 @@
+/*
+ * arraytostring.java
+ * Copyright 2021 Qunhe Tech, all rights reserved.
+ * Qunhe PROPRIETARY/CONFIDENTIAL, any form of usage is subject to approval.
+ */
+
+package day04;
+
+import java.util.Arrays;
+
+/**
+ * Function: 
+ *
+ * @author 云铮
+ * @date 2021/1/23
+ */
+public class arraytostring {
+    public static void main(String[] args) {
+        int [] a = new int[]{1,2,3,4,5,6,7,8};
+        String b =  Arrays.toString(a);
+        System.out.println(b);
+
+    }
+
+}
---
+++ src/day04/zengqiangbianli.java
@@ -0,0 +1,24 @@
+/*
+ * zengqiangbianli.java
+ * Copyright 2021 Qunhe Tech, all rights reserved.
+ * Qunhe PROPRIETARY/CONFIDENTIAL, any form of usage is subject to approval.
+ */
+
+package day04;
+
+/**
+ * Function: 数组增强遍历
+ *
+ * @author 云铮
+ * @date 2021/1/17
+ */
+public class zengqiangbianli {
+    public static void main(String[] args) {
+        int [] a = new int[] {1,222,34,56,322,12};
+        for (int each:a
+        ) {
+            System.out.println(each);
+        }
+
+    }
+}
